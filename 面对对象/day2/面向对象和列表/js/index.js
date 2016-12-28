e5c8cb908b1735617ad162bd57789e0c6003cca9
HowardTangHw <2379034@qq.com>
2016-12-28 19:10:55 +0800

    2016年12月28日19:10:53

--- 面对对象/day2/面向对象和列表/js/index.js
+++ 面对对象/day2/面向对象和列表/js/index.js
@@ -1,6 +1,6 @@
 //使用面向对象形式开发列表页面
 
-function Product() {
+function Product(data) {
     //名称
     this.name = "";
     //原价
@@ -13,66 +13,98 @@ function Product() {
     this.sum = 0;
     //折扣
     this.discount = 0;
+    // 接收后台给我的json数据
+    this.data = data;
 }
 Product.prototype = {
-        //绑定单个产品
-        bindDom: function() {
-            var str = "";
-            str += "<dl>";
-            str += "<dt><a><img src='" + this.image + "' width='384' height='225' /></a></dt>";
-            str += "<dd>";
-            str += "<span><a><em>" + this.discount + "折/</em>" + this.name + "</a></span>";
-            str += "</dd>";
-            str += "<dd class='price'>";
-            str += "<em>$" + this.price + "</em>";
-            str += "<del>$" + this.SourcePrice + "</del>";
-            str += "<i>销量：" + this.sum + "</i>";
-            str += "</dd>";
-            str += "</dl>";
+    //绑定单个产品
+    bindDom: function() {
+        var str = "";
+        str += "<dl>";
+        str += "<dt><a><img src='" + this.image + "' width='384' height='225' /></a></dt>";
+        str += "<dd>";
+        str += "<span><a><em>" + this.discount + "折/</em>" + this.name + "</a></span>";
+        str += "</dd>";
+        str += "<dd class='price'>";
+        str += "<em>$" + this.price + "</em>";
+        str += "<del>$" + this.SourcePrice + "</del>";
+        str += "<i>销量：" + this.sum + "</i>";
+        str += "</dd>";
+        str += "</dl>";
 
-            //一定要记住return  不然就会undefined
-            return str;
-        },
-        bindEvents: function() {},
-        //绑定多个产品
-        bingDoms: function(products) {
-            var str = '';
-            for (var i = 0; i < products.length; i++) {
-                str += products[i].bindDom();
-            }
-            $('#container').html(str);
-        },
-        init: function(products) {
-            this.bingDoms(products);
-            this.bindEvents();
+        //一定要记住return  不然就会undefined
+        return str;
+    },
+    bindEvents: function() {},
+    //绑定多个产品
+    bingDoms: function() {
+        var str = '';
+        for (var i = 0; i < this.data.length; i++) {
+            str += this.data[i].bindDom();
         }
+        $('#container').html(str);
+    },
+    init: function() {
+        this.bingDoms();
+        this.bindEvents();
     }
-    //////////模拟后台数据
-    //模拟单个
-var conditioner = new Product();
-conditioner.name = 'Aussie美国袋鼠丰盈蓬松护发素400ml';
-conditioner.SourcePrice = '120.00';
-conditioner.discount = '3.5';
-conditioner.image = 'img/boutque01_r2_c2.jpg';
-conditioner.sum = "2000";
-conditioner.price = "42.00";
+}
 
 
-//模拟多个
-var treatment = new Product();
-treatment.name = 'Aussie美国袋鼠丰盈蓬松护发素400ml';
-treatment.SourcePrice = '120.00';
-treatment.discount = '3.5';
-treatment.image = 'img/boutque01_r2_c2.jpg';
-treatment.sum = "2000";
-treatment.price = "42.00";
-
-
-
-
-
-var list = [conditioner, conditioner, conditioner, conditioner, treatment, treatment, treatment, treatment, treatment, treatment, treatment];
+///后台给的Json
+var json = [{
+    name: "Aussie美国袋鼠丰盈蓬松护发素400ml",
+    SourcePrice: "120.00",
+    discount: "3.5",
+    image: "img/boutque01_r2_c2.jpg",
+    sum: "2000",
+    price: "42.00"
+}, {
+    name: "Aussie美国袋鼠丰盈蓬松护发素400ml",
+    SourcePrice: "120.00",
+    discount: "3.5",
+    image: "img/boutque01_r2_c2.jpg",
+    sum: "2000",
+    price: "42.00"
+}, {
+    name: "Aussie美国袋鼠丰盈蓬松护发素400ml",
+    SourcePrice: "120.00",
+    discount: "3.5",
+    image: "img/boutque01_r2_c2.jpg",
+    sum: "2000",
+    price: "42.00"
+}, {
+    name: "Aussie美国袋鼠丰盈蓬松护发素400ml",
+    SourcePrice: "120.00",
+    discount: "3.5",
+    image: "img/boutque01_r2_c2.jpg",
+    sum: "2000",
+    price: "42.00"
+}, {
+    name: "Aussie美国袋鼠丰盈蓬松护发素400ml",
+    SourcePrice: "120.00",
+    discount: "3.5",
+    image: "img/boutque01_r2_c2.jpg",
+    sum: "2000",
+    price: "42.00"
+}];
 
 
 //面向对象编程
-conditioner.init(list);
+//将json字符串转换为json对象
+function transform(json) {
+    var list = [];
+    for (var i = 0; i < json.length; i++) {
+        var p = new Product();
+        p.name = json[i].name;
+        p.SourcePrice = json[i].SourcePrice;
+        p.discount = json[i].discount;
+        p.image = json[i].image;
+        p.sum = json[i].sum;
+        p.price = json[i].price;
+        list.push(p);
+    }
+    return list;
+}
+var p = new Product(transform(json));
+p.init();
