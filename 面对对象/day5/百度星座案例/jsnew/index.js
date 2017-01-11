72f459586c337698e3189b442d504c7a8e351a7b
HowardTangHw <2379034@qq.com>
2017-01-11 17:40:49 +0800

    2017年1月11日17:40:47

--- 面对对象/day5/百度星座案例/jsnew/index.js
+++ 面对对象/day5/百度星座案例/jsnew/index.js
@@ -11,10 +11,11 @@ var data = [
     { name: '白羊座', time: '3.21-4.19' },
     { name: '白羊座', time: '3.21-4.19' },
     { name: '白羊座', time: '3.21-4.19' },
+    { name: '白羊座', time: '3.21-4.19' },
 ];
 
 //循环
 
 for (var i = 0; i < data.length; i++) {
-    new Xingzuo(data[i], i).init();
+    new Xingzuo(data[i], i);
 }
